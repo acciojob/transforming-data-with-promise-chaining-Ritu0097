abf53546c9799e036a16579a704f3141b36e00e1
Ritesh Kishor Chaudhari <100477678+Ritu0097@users.noreply.github.com>
2024-03-09 14:13:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,18 +13,21 @@ document.getElementById('btn').addEventListener('click', function() {
             }, 1000);
         });
     }).then((result) => {
+		document.getElementById('output').textContent = `Result: ${result}`;
         return new Promise((resolve, reject) => {
             setTimeout(() => {
                 resolve(result - 3);
             }, 1000);
         });
     }).then((result) => {
+		document.getElementById('output').textContent = `Result: ${result}`;
         return new Promise((resolve, reject) => {
             setTimeout(() => {
                 resolve(result / 2);
             }, 1000);
         });
     }).then((result) => {
+		document.getElementById('output').textContent = `Result: ${result}`;
         return new Promise((resolve, reject) => {
             setTimeout(() => {
                 resolve(result + 10);
